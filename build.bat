88aa0668ad6bfe0719ca273e4bca551192a914f3
dommilosz <dommilosz@gmail.com>
2022-02-15 20:41:04 +0100

    automatic creation of build directory, make whole script fail when one thing fails

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
-cd build
-cmake -G "Unix Makefiles" ..
-make vga_graphics_card 
+IF NOT EXIST build/ (
+   echo build/ doesn't exist. Creating...
+   mkdir build
+)
+cd build && cmake -G "Unix Makefiles" .. && make vga_graphics_card 
